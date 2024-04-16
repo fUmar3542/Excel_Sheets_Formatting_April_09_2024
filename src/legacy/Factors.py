78f132376ac8d03fbd06f444a357e5f523c7c64c
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-04-16 15:35:58 +0500

    Pre Error Handling Factors.py

--- src/legacy/Factors.py
+++ src/legacy/Factors.py
@@ -2,16 +2,22 @@
 import logging
 
 import pandas as pd
+from src.handles.exception_handling import MyExceptions
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
-#import _pydevd_bundle
+
+# import _pydevd_bundle
+
 
 def calculate_position_betas(
-    factor_returns: pd.DataFrame,
-    position_returns: pd.DataFrame,
+        factor_returns: pd.DataFrame,
+        position_returns: pd.DataFrame,
 ) -> pd.DataFrame:
+    beta_all = None
+    factor_returns_ortho = None
+    beta = None
     '''
     Calculates the positions's beta 
     Equity beta is beta vs first column of factor_returns
@@ -27,101 +33,137 @@ def calculate_position_betas(
             each factor, with position names as index and factor names as columns.
     '''
 
-    # Position level beta to each factor
-    factor_returns_long = factor_returns\
-        .reset_index()\
-        .melt(
+    try:
+        # Position level beta to each factor
+        factor_returns_long = factor_returns \
+            .reset_index() \
+            .melt(
             id_vars='date',
             var_name='factor',
             value_name='factor_return',
         )
-    # print(factor_returns_long)
+        # print(factor_returns_long)
 
-    position_returns_long = position_returns\
-        .reset_index()\
-        .rename(
+        position_returns_long = position_returns \
+            .reset_index() \
+            .rename(
             columns={
                 'return': 'position_return',
                 'TradeDate': 'date',
                 'VaRTicker': 'position'
             }
-        )\
-        .melt(
+        ) \
+            .melt(
             id_vars='date',
             var_name='position',
             value_name='position_return',
         )
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during calculating position's beta\n\n" + str(
+                                      ex))
 
-    # Merge factor and position returns
-    merged_returns = pd.merge(factor_returns_long, position_returns_long, on='date', how='inner')
-    mr = merged_returns.reset_index().copy()    #fact_eq = mr[mr['factor']==factor_eq][['date','factor_return']].rename(columns={"factor_return":"eq_return"})
+    try:
+        # Merge factor and position returns
+        merged_returns = pd.merge(factor_returns_long, position_returns_long, on='date', how='inner')
+        mr = merged_returns.reset_index().copy()  # fact_eq = mr[mr['factor']==factor_eq][['date','factor_return']].rename(columns={"factor_return":"eq_return"})
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during merging of factor returns and position returns\n\n" + str(
+                                      ex))
 
-    # natural beta vs factors 
-    covar_position_fact = mr.groupby(['position','factor'])[['position_return', 'factor_return']].cov()
-    var_df = covar_position_fact.iloc[1::2, 1].reset_index().drop(columns=['level_2']).set_index(['position','factor'])
-    covar_df = covar_position_fact.iloc[::2, 1].reset_index().drop(columns=['level_2']).set_index(['position','factor'])
-    beta = covar_df/var_df # SLR beta = covariance / variance regressor
-    beta.reset_index(inplace=True)
-    beta.rename(columns={'factor_return':'beta'},inplace=True)
-    beta = pd.pivot_table(data = beta, values='beta',columns='factor', index='position')
-    # put columns in same order as factors + rename first column 'ID'
-    beta = beta[factor_returns.columns]
-    beta.reset_index(inplace=True)
-    beta.rename(columns={'position':'ID'},inplace=True)
-    
-    # get Equity returns
-    factor_eq = factor_returns.columns[0] # the Equity Factor is always the first column, cf helper.imply_smb_gmv
-    fact_eq = mr[mr['factor']==factor_eq][['date','factor','factor_return']].drop_duplicates().rename(columns={"factor":factor_eq,"factor_return":"eq_return"})
-    mr = pd.merge(mr,fact_eq, on = 'date', how='left')
+    try:
+        # natural beta vs factors
+        covar_position_fact = mr.groupby(['position', 'factor'])[['position_return', 'factor_return']].cov()
+        var_df = covar_position_fact.iloc[1::2, 1].reset_index().drop(columns=['level_2']).set_index(
+            ['position', 'factor'])
+        covar_df = covar_position_fact.iloc[::2, 1].reset_index().drop(columns=['level_2']).set_index(
+            ['position', 'factor'])
+        beta = covar_df / var_df  # SLR beta = covariance / variance regressor
+        beta.reset_index(inplace=True)
+        beta.rename(columns={'factor_return': 'beta'}, inplace=True)
+        beta = pd.pivot_table(data=beta, values='beta', columns='factor', index='position')
+        # put columns in same order as factors + rename first column 'ID'
+        beta = beta[factor_returns.columns]
+        beta.reset_index(inplace=True)
+        beta.rename(columns={'position': 'ID'}, inplace=True)
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during calculation of SLR beta\n\n" + str(
+                                      ex))
+    try:
+        # get Equity returns
+        factor_eq = factor_returns.columns[0]  # the Equity Factor is always the first column, cf helper.imply_smb_gmv
+        fact_eq = mr[mr['factor'] == factor_eq][['date', 'factor', 'factor_return']].drop_duplicates().rename(
+            columns={"factor": factor_eq, "factor_return": "eq_return"})
+        mr = pd.merge(mr, fact_eq, on='date', how='left')
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during getting equity returns\n\n" + str(
+                                      ex))
+    try:
+        # beta position vs Equity
+        # print(_pydevd_bundle.pydevd_constants.PYDEVD_WARN_EVALUATION_TIMEOUT )
+        covar_position_eq = mr[mr['factor'] == factor_eq].groupby(['position'])[['position_return', 'eq_return']].cov()
+        var_eq = fact_eq['eq_return'].var()  # covar_byposition.iloc[1,1]
+        beta_eq = covar_position_eq.iloc[::2, 1] / var_eq  # SLR beta = covariance / variance regressor
+        beta_eq.index = [(factor_eq, x) for (x, _) in beta_eq.index]
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during calaculation of beta returns vs equity\n\n" + str(
+                                      ex))
+    try:
+        # adjust factor return: substract beta*equity return
+        covar_byfactor = mr.groupby(['factor'])[['factor_return', 'eq_return']].cov()
+        beta_fact_eq = covar_byfactor.iloc[::2, 1] / var_eq  # SLR beta = covaraince / variance regressor
+        beta_fact_eq.index = [x for (x, _) in beta_fact_eq.index]
+        beta_fact_eq.name = 'beta_fact_vs_eq'
+        mr = pd.merge(mr, beta_fact_eq, left_on='factor', right_index=True)
+        mr.loc[mr['factor'] == factor_eq, 'beta_fact_vs_eq'] = 0  # we don't want to orthogonalise the Equity factor ==> put its beta to 0
+        mr['factor_return'] = mr['factor_return'] - mr['beta_fact_vs_eq'] * mr['eq_return']
 
-    # beta position vs Equity
-    #print(_pydevd_bundle.pydevd_constants.PYDEVD_WARN_EVALUATION_TIMEOUT )
-    covar_position_eq = mr[mr['factor']==factor_eq].groupby(['position'])[['position_return', 'eq_return']].cov()
-    var_eq = fact_eq['eq_return'].var() #covar_byposition.iloc[1,1]
-    beta_eq = covar_position_eq.iloc[::2, 1] / var_eq   # SLR beta = covariance / variance regressor
-    beta_eq.index = [ (factor_eq,x) for (x,_) in beta_eq.index]
-    
-    #adjust factor return: substract beta*equity return 
-    covar_byfactor = mr.groupby(['factor'])[['factor_return', 'eq_return']].cov()
-    beta_fact_eq = covar_byfactor.iloc[::2, 1] / var_eq   # SLR beta = covaraince / variance regressor    
-    beta_fact_eq.index = [ x for (x,_) in beta_fact_eq.index]
-    beta_fact_eq.name = 'beta_fact_vs_eq'
-    mr = pd.merge(mr,beta_fact_eq, left_on='factor', right_index=True)
-    mr.loc[mr['factor']==factor_eq,'beta_fact_vs_eq']=0  # we don't want to orthogonalise the Equity factor ==> put its beta to 0 
-    mr['factor_return']=mr['factor_return']-mr['beta_fact_vs_eq']*mr['eq_return']
+        # regress positions vs factors
+        covar_byposition = mr.groupby(['factor', 'position'])[['position_return', 'factor_return']].cov()
+        var_eq = covar_byposition.iloc[1::2, 1]
+        beta_fact = covar_byposition.iloc[1::2, 0] / var_eq  # SLR beta = covaraince / variance regressor
+        beta_fact.index = [(x, y) for (x, y, _) in beta_fact.index]
+        beta_fact = beta_fact[[x for x in beta_fact.index if x[0] != factor_eq]]  # remove the Equity as a factor
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during adjusting factor returns\n\n" + str(
+                                      ex))
+    try:
+        # merge the 2 betas series
+        beta_all = pd.concat([beta_eq, beta_fact], axis=0)
+        beta_all = pd.DataFrame(beta_all, columns=['beta'])
+        beta_all.reset_index(inplace=True)
+        beta_all['factor'] = beta_all['index'].apply(lambda x: x[0])
+        beta_all['position'] = beta_all['index'].apply(lambda x: x[1])
+        beta_all.drop(columns='index', inplace=True)
+        beta_all.set_index('position', inplace=True)
+        beta_all = pd.pivot_table(data=beta_all, values='beta', columns='factor', index='position')
+        # arrange columns in same order as factors + name first column 'ID'
+        beta_all = beta_all[factor_returns.columns]
+        beta_all.index.name = 'ID'
+        beta_all.reset_index(inplace=True)
 
-    # regress positions vs factors 
-    covar_byposition = mr.groupby(['factor','position'])[['position_return', 'factor_return']].cov()
-    var_eq = covar_byposition.iloc[1::2,1]
-    beta_fact = covar_byposition.iloc[1::2, 0] / var_eq   # SLR beta = covaraince / variance regressor
-    beta_fact.index = [ (x,y) for (x,y,_) in beta_fact.index]
-    beta_fact= beta_fact[[x for x in beta_fact.index if x[0]!=factor_eq]]  # remove the Equity as a factor 
-    
-    # merge the 2 betas series
-    beta_all = pd.concat([beta_eq,beta_fact],axis=0)
-    beta_all = pd.DataFrame(beta_all,columns=['beta'])
-    beta_all.reset_index(inplace=True)
-    beta_all['factor']=beta_all['index'].apply(lambda x:x[0])
-    beta_all['position']=beta_all['index'].apply(lambda x:x[1])
-    beta_all.drop(columns='index',inplace=True)
-    beta_all.set_index('position',inplace=True)
-    beta_all = pd.pivot_table(data = beta_all,values='beta',columns='factor', index='position')    
-    # arrange columns in same order as factors + name first column 'ID'
-    beta_all = beta_all[factor_returns.columns]
-    beta_all.index.name = 'ID'
-    beta_all.reset_index(inplace=True)
-    
-    factor_returns_ortho = pd.pivot_table(mr, values = 'factor_return', index = 'date', columns = 'factor' )
-    
-    return beta_all, factor_returns_ortho[factor_returns.columns], beta
+        factor_returns_ortho = pd.pivot_table(mr, values='factor_return', index='date', columns='factor')
+    except Exception as ex:
+        MyExceptions.show_message(tab='Factors.py',
+                                  message="Following exception occurred during merging of beta series\n\n" + str(
+                                      ex))
+    finally:
+        return beta_all, factor_returns_ortho[factor_returns.columns], beta
+
 
 def calculate_beta_factor_numerator(merged_returns):
     '''calculates the numerator to calculate factor betas'''
 
-    return merged_returns\
-        .groupby(['factor', 'position'])[['position_rf', 'factor_rf']]\
-        .cov()\
-        .iloc[::2, 1]
+    return merged_returns \
+               .groupby(['factor', 'position'])[['position_rf', 'factor_rf']] \
+               .cov() \
+               .iloc[::2, 1]
+
 
 def calculate_beta_factors_denominator(merged_returns):
     '''calculates the denominator to calculate factor betas'''
